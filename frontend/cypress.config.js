a5b11d9d20d547bdb9d3233083b7327ef245f6e7
Reveling-h <158114814+Reveling-h@users.noreply.github.com>
2024-11-03 19:18:01 -0800

    coverage

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "cypress";
+import codeCoverage from '@cypress/code-coverage/task.js';
 
 export default defineConfig({
-  // e2e: {
-  //   setupNodeEvents(on, config) {
-  //     // implement node event listeners here
-  //   },
-  // },
   e2e: {
     baseUrl: "http://localhost:5173/",
+    setupNodeEvents(on, config) {
+      codeCoverage(on, config);
+      return config;
+    },
   },
 
   component: {
